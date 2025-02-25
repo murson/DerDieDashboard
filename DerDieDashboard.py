fffd0a63ae0219c1a811c7795e0c0644725cac26
murson <mikeurson@gmail.com>
2025-02-25 08:18:53 +0100

    Fixed text in main page.

--- DerDieDashboard.py
+++ DerDieDashboard.py
@@ -14,7 +14,7 @@ key_endings = 'data/key_endings_v2.pkl'
 key_stats = 'data/key_stats.pkl'
 
 st.sidebar.subheader("The German gender demystifier you didn't know you needed")
-st.sidebar.text('Finally, a way to more precisely visualize the relationships between German noun endings, and their genders.')
+st.sidebar.text('Finally, a way to more precisely visualize the relationships between German noun endings, and their genders. With this tool, you can easily learn the genders of over 55% of all 75k German nouns, at 90% accuracy!')
 st.sidebar.text('Click on a barplot to explore the words and exceptions associated with each ending.')
 st.sidebar.text('The little number above each plot indicates the accuracy of that ending (i.e. % of words belonging to that ending\'s majority gender.)')
 st.sidebar.text('The tables below the plots present lists of words belonging to each gender')
